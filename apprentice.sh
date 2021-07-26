a471104404dd54829b283ea7c4a20e8e5932f6d4
Kordell Hutchins <43355784+hazarky@users.noreply.github.com>
2021-07-26 13:07:55 -0400

    Update apprentice.sh

--- apprentice.sh
+++ apprentice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Defining all the parameters
-BACKUPTIME='date +%b-%d-%y-%H-%M'
+BACKUPTIME=`date +%b-%d-%y-%H-%M`
 DESTINATION=/root/secure/backup-$BACKUPTIME.tar.gz
 IPTABLES=/etc/sysconfig/network-scripts/iptables.rules
 #Check if the secure file exist
@@ -17,22 +17,29 @@ FAIL=/var/log/faillog
 FAIL2=/var/log/btmp
 LOGOUT=/var/log/wtmp
 LASTLOG=/var/log/lastlog
-SMB=/srv/samba/*
-DNSC=/etc/named/*
-POSTM=/etc/postfix/*
-SQLC=/etc/mysql/*
 if [[ -e /var/log/mysql.log ]]; then
     SQLL=/var/log/mysql.log
 elif [[ -e /var/log/mysqld.log ]]; then
     SQLL=/var/log/mysqld.log
 fi
-if [[  -d /etc/httpd/ ]]; then
-    HTTPC=/etc/httpd/*
-else
-    HTTPC=/etc/apache2/*
+SMB=/srv/samba/*
+if [[ -d /etc/bind ]]; then
+    DNSC=/etc/bind
+elif [[ -e /etc/named.conf ]]; then
+    DNSC=/etc/named.conf
+    DNSD=/var/named
 fi
-HTTPD=/var/www/*
-HTTPV=/var/log/httpd/*
+POSTM=/etc/postfix/*
+SQLC=/etc/mysql/*
+if [[  -d /etc/httpd ]]; then
+    HTTPC=/etc/httpd
+
+elif [[ -d /etc/apache2 ]]; then
+    HTTPC=/etc/apache2
+
+fi
+HTTPD=/var/www
+HTTPV=/var/log/httpd
 #Grabbing logging info
 cp $LASTLOG $HOMELOGS
 cp $DMESG $HOMELOGS
@@ -41,15 +48,41 @@ cp $FAIL $HOMELOGS
 cp $FAIL2 $HOMELOGS
 cp $LASTLOG $HOMELOGS
 cp $SQLL $HOMELOGS
-cp $IPTABLES $HOMELOGS
+cp $LOGOUT $HOMELOGS
+if [[ -e $SQLL ]]; then
+    cp $SQLL $HOMELOGS
+fi
+if [[ -e $IPTABLES ]]; then
+    cp $IPTABLES $HOMELOGS
+fi
 #Grabbing service files
-cp -R $HTTPC $HOMESERV
-cp -R $HTTPD $HOMESERV
-cp -R $HTTPV $HOMESERV
-cp -R $SQLC $HOMESERV
-cp -R $SMB $HOMESERV
-cp -R $DNSC $HOMESERV
-cp -R $POSTM $HOMESERV
+if [[ -d $HTTPC ]]; then
+    cp -R $HTTPC $HOMESERV
+fi
+if [[ -d $HTTPD ]]; then 
+    cp -R $HTTPD $HOMESERV
+fi
+if [[ -d $HTTPV ]]; then
+    cp -R $HTTPV $HOMESERV
+fi 
+if [[ -d $SQLC ]]; then
+    cp -R $SQLC $HOMESERV
+fi
+if [[ -d $SMB ]]; then
+    cp -R $SMB $HOMESERV
+fi
+if [[ -d $DNSC ]]; then
+    cp -R $DNSC $HOMESERV
+fi 
+if [[ -d $DNSD ]]; then
+    cp -R $DNSD $HOMESERV
+fi
+if [[ -d $POSTM ]]; then
+    cp -R $POSTM $HOMESERV
+fi
+if [[ -d  $SSHD ]]; then
+    cp -R $SSHD $HOMESERV
+fi
 tar -cpzf $DESTINATION $HOME #create the backup
 chmod 600 $DESTINATION #Apply respecting permissions
 #Apply password encryption
